2b726d903bf88cb0c312a2b7c1f25f3486e28767
Ryan Holt <ryan.holt@gmail.com>
2014-10-15 13:51:26 -0500

    commit 10/15/2014

--- src/main/java/com/sageburner/im/server/controller/SpringServiceController.java
+++ src/main/java/com/sageburner/im/server/controller/SpringServiceController.java
@@ -1,16 +1,21 @@
 package com.sageburner.im.server.controller;
 
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import com.sageburner.im.server.model.User;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/service/greeting")
 public class SpringServiceController {
+//    @RequestMapping(value = "/{name}", method = RequestMethod.GET)
+//    public String getGreeting(@PathVariable String name) {
+//        String result="Hello "+name;
+//        return result;
+//    }
+
     @RequestMapping(value = "/{name}", method = RequestMethod.GET)
-    public String getGreeting(@PathVariable String name) {
-        String result="Hello "+name;
-        return result;
+    public @ResponseBody User getUser(@PathVariable String name){
+        User user = new User();
+        user.setUsername(name);
+        return user;
     }
 }
