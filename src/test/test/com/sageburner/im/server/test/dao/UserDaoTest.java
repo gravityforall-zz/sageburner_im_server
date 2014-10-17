09adc33dc16fe4a65765271fd1b93e11ec7761f7
Ryan Holt <ryan.holt@gmail.com>
2014-10-17 02:15:43 -0500

    commit 10/17/2014 part 2

---
+++ src/test/test/com/sageburner/im/server/test/dao/UserDaoTest.java
@@ -0,0 +1,24 @@
+package com.sageburner.im.server.test.dao;
+
+import com.sageburner.im.server.model.User;
+import org.hibernate.Session;
+import org.junit.Test;
+
+import java.util.List;
+
+/**
+ * Created by Ryan on 10/17/2014.
+ */
+public class UserDaoTest {
+
+    @Test
+    public void addUserTest(User user) {
+//        Session session = this.sessionFactory.getCurrentSession();
+//        session.persist(user);
+//
+//        User userTest = (User) session.load(User.class, user.getUsername());
+//
+//        assertEquals(userTest.getUsername(), user.getUsername());
+//        assertEquals(userTest.getUpdatedAt(), user.getUpdatedAt());
+    }
+}
