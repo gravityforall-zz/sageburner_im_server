26fb2ffa212a0b2508cd58713599df62670b4aa7
Ryan Holt <ryan.holt@gmail.com>
2014-12-20 17:41:00 -0600

    commit 12/20/2014

---
+++ src/main/com/sageburner/im/server/jpbc/IBEcollector.java
@@ -0,0 +1,11 @@
+package com.sageburner.im.server.jpbc;
+
+/**
+ * Used to collect public keys from keyservers and encrypt a message
+ * 
+ * @author John
+ * 
+ */
+public class IBEcollector {
+	
+}
